ba37b6b4b0522ade46492ce7dd2c5b685daf3e17
surafel <72102647+surafel58@users.noreply.github.com>
2023-02-16 12:24:56 +0300

    Solution for Insertion sort prolem from leetcode

---
+++ Insertion sort.py
@@ -0,0 +1,35 @@
+def insertionSort1(n, arr):
+    # Write your code here
+    last_element = arr[-1]
+    i = len(arr) - 1
+    
+    while i > 0:
+      
+      if arr[i-1] > last_element:
+        arr[i] = arr[i-1]
+        for j in range(n):
+            print(arr[j], end=" ")
+        print()
+        
+      else:
+          arr[i] =  last_element
+          for j in range(n):
+            print(arr[j], end=" ")
+          print()
+          break
+      
+      i -= 1
+      
+    if i==0:
+        arr[0] =  last_element
+        for j in range(n):
+            print(arr[j], end=" ")
+        print() 
+    
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
