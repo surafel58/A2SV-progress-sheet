6f9e1cec2e61c2a33ccf30572c0ce62c5ebb056b
surafel <72102647+surafel58@users.noreply.github.com>
2023-03-15 11:34:06 +0300

    Update Find Kth Bit in Nth Binary String.py

--- Camp Progress sheet/Find Kth Bit in Nth Binary String.py
+++ Camp Progress sheet/Find Kth Bit in Nth Binary String.py
@@ -1,3 +1,4 @@
+#naive approach
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
         bits = self.generateString(n, k)
@@ -19,6 +20,28 @@ class Solution:
 
         return leftHalf + "1" + rightHalf
 
+#best way
+class Solution:
+
+    def findKth(self, n, k):
+        
+        if n == 1:
+            return 0
+
+        prevLen = (2 ** (n-1)) - 1
+        
+        if k - (prevLen) == 1:
+            return 1
+
+        if k <= (prevLen):
+            ans = self.findKth(n-1, k)
+        else:
+            ans = 1 - self.findKth(n-1, 2 * (prevLen + 1) - k)
+
+        return ans
+
+    def findKthBit(self, n: int, k: int) -> str:
+        return str(self.findKth(n,k))
 
         
-             
+
